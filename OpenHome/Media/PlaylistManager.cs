de485b12fca2ea76c58ce59318f9c194ffba8055
David Drysdale-Wilson <david.drysdale-wilson@linn.co.uk>
2012-02-24 13:59:28 +0000

    Fixed metadata action return value.

--- OpenHome/Media/PlaylistManager.cs
+++ OpenHome/Media/PlaylistManager.cs
@@ -12,7 +12,7 @@ namespace OpenHome.Media
         public PlaylistManager(NetworkAdapter aAdapter, string aRootPath, string aMachineName, string aManufacturer, string aManufacturerUrl, string aModel, string aModelUrl, IResourceManager aResourceManager, string aIconUri)
         {
             string upnpname = string.Format("{0} {1} PlaylistManager ({2})", aManufacturer, aModel, aMachineName);
-            string name = string.Format("{0} {1} ({2})", aManufacturer, aModel, aMachineName);
+            string name = string.Format("{0} {1} PlaylistManager ({2})", aManufacturer, aModel, aMachineName);
             string udn = string.Format("{0}-{1}-PlaylistManager-{2}", aManufacturer, aModel, aMachineName);
 
             string resourcePath = Path.Combine(aRootPath, "PlaylistManager");
